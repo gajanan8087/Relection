54c492b8566a49ae98156b4418abaabd073a1810
hp <hp@DESKTOP-LE9U86J>
2020-01-09 13:22:32 +0530

    Added some more checks..

--- src/com/jbk/TestCase1.java
+++ src/com/jbk/TestCase1.java
@@ -3,7 +3,7 @@ package com.jbk;
 public class TestCase1 {
 	public static void main(String[] args) {
 		String ans ="Yes";
-		if (ans== "Yes") {
+		if (ans== "Yes" || ans =="yes" || ans=="Y" || ans =="y") {
 			System.out.println("Thank you");
 		}
 		else {
