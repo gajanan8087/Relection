d946060bfb289d4243a0ac86299804d50f5c4080
hp <hp@DESKTOP-LE9U86J>
2020-01-09 12:48:20 +0530

    Commiting TestCase1 to BranchX

---
+++ src/com/jbk/TestCase1.java
@@ -0,0 +1,14 @@
+package com.jbk;
+
+public class TestCase1 {
+	public static void main(String[] args) {
+		String ans ="Yes";
+		if (ans== "Yes") {
+			System.out.println("Thank you");
+		}
+		else {
+			System.out.println("Go to hell");
+		}
+	}
+
+}
